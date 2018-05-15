c85f847533404095f2b8114032e725e271c3caa9
Guoyin Wang <guoyin.wang@duke.edu>
2018-05-15 11:57:25 -0400

    correct model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.contrib import layers
 
 def embedding(features, opt, prefix='', is_reuse=None):
     """Customized function to transform batched x into embeddings."""
